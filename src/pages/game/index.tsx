7faf44b25706367878832fb64cb95ff81a388cd7
Luciano Raso <luciano.raso@7r1ck.com>
2024-07-16 14:30:08 -0300

    - mobile fixes

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -2,16 +2,16 @@ import { useParams, useNavigate } from "react-router-dom";
 import { Box, Container, Grid, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 
-import Layout from "components/templates/layout";
-import ActionsBar from "components/organisms/actionsbar";
-
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 import Button from "components/atoms/buttons/base";
-import ButtonDefault from "components/atoms/buttons/default";
+import Layout from "components/templates/layout";
+import ActionsBar from "components/organisms/actionsbar";
+import GameBar from "components/organisms/gameBar";
+
+import rotateIcon from "assets/icons/rotate.svg";
 
 import styled from "./styled.module.scss";
-import keyIcon from "assets/icons/key.svg";
 
 const Game = () => {
   const { i18n } = useTranslation();
@@ -27,6 +27,15 @@ const Game = () => {
       <Container maxWidth={false} disableGutters={true}>
         <ActionsBar />
 
+        <Box className={styled.mobileRotate}>
+          <Box>
+            <img src={rotateIcon} alt="rotate" />
+            <Typography>
+              To have a better gaming experience, please rotate your phone horizontally.
+            </Typography>
+          </Box>
+        </Box>
+
         <Box className={styled.backButton}>
           <Button onClick={handleClick}>
             <ArrowBackIcon /> Back
@@ -39,46 +48,9 @@ const Game = () => {
               game: {idgame}
             </Typography>
           </Grid>
+
           <Grid item xs="auto" container className={styled.rightCol}>
-            <Box className={styled.dotted}></Box>
-            <Grid item container xs={12} className={styled.container}>
-              <Grid item xs={12} className={styled.info}>
-                <Typography variant="h5" className={styled.title}>
-                  INSTRUCTIONS
-                </Typography>
-
-                <Typography className={styled.text}>
-                  The original Gacha game! Get your keys out, and get ready to play for randomized
-                  loot where every key grants you a prize.
-                </Typography>
-              </Grid>
-
-              <Grid item xs={12} className={styled.balanceContainer}>
-                <Typography variant="h5" className={styled.title}>
-                  YOUR KEYS <br /> BALANCE
-                </Typography>
-
-                <Box className={styled.balance}>
-                  <img src={keyIcon} alt="key" height={"40px"} />
-                  <Typography className={styled.text}>7</Typography>
-                </Box>
-              </Grid>
-
-              <Grid item xs={12} className={styled.keyContainer}>
-                <Typography variant="h5" className={styled.title}>
-                  PLAY PRICE
-                </Typography>
-
-                <Box className={styled.keys}>
-                  <img src={keyIcon} alt="key" />
-                  <Typography className={styled.text}>1 KEY</Typography>
-                </Box>
-              </Grid>
-            </Grid>
-
-            <Grid item xs={12} className={styled.footer}>
-              <ButtonDefault>PLAY</ButtonDefault>
-            </Grid>
+            <GameBar />
           </Grid>
         </Grid>
       </Container>
