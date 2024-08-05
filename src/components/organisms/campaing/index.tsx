be52b626c1b9bd93b9d881151d1b0cf4e837b11a
Luciano Raso <luciano.raso@7r1ck.com>
2024-08-05 16:01:45 -0300

    - minor fix

--- src/components/organisms/campaing/index.tsx
+++ src/components/organisms/campaing/index.tsx
@@ -20,7 +20,7 @@ const Campaing = ({ campaing, handleClose }: any) => {
         script.src = "https://widgets.claimr.io/claimr.min.js";
         script.id = "claimr-script";
         script.setAttribute("data-addons", "sup,sur");
-        script.setAttribute("data-campaign", "me3-alpha-journey");
+        script.setAttribute("data-campaign", campaing.id);
         script.setAttribute("data-autoresize", "true");
         script.setAttribute("data-container", campaing.id);
         script.setAttribute("data-organization", "me3");
