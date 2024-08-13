96db4d51d3638ba552bea803432306be643f3a32
Luciano Raso <luciano.raso@7r1ck.com>
2024-08-13 12:32:05 -0300

    - fix nickname

--- src/components/organisms/menu/sections/profile.tsx
+++ src/components/organisms/menu/sections/profile.tsx
@@ -43,6 +43,12 @@ const Profile = ({ setOpenProfile }: any) => {
       return;
     }
 
+    const regex = /^[^\s]{1,25}$/;
+    if (!regex.test(nickname)) {
+      setOnAlert({ show: true, severity: "error", msg: "Nickname must be 1-25 characters long and contain no spaces" });
+      return;
+    }
+    
     await customAxios()
       .post("/user/updateprofile", {
         nickname,
@@ -93,7 +99,7 @@ const Profile = ({ setOpenProfile }: any) => {
               value={nickname}
               onChange={handleChange}
               autoComplete="off"
-              inputProps={{ maxLength: 30 }}
+              inputProps={{ maxLength: 25 }}
               InputProps={{
                 endAdornment: (
                   <>
