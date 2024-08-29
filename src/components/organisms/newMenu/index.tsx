69ecb53d59bbad6a2f5986d923bbcc9c5fe237fd
Luciano Raso <luciano.raso@7r1ck.com>
2024-08-29 17:19:30 -0300

    - minor fixes

--- src/components/organisms/newMenu/index.tsx
+++ src/components/organisms/newMenu/index.tsx
@@ -52,16 +52,17 @@ const MainPanel = ({ showBack = false, goToMap, setGame, setCampaing }: any) => 
           />
         </SectionItem>
 
-        {/* submenu Earn Points */}
+        {/* subsection Earn Points */}
         <SectionItem open={openPoints}>
           <EarnPoints setOpen={setOpenPoints} setGame={setGame} setCampaing={setCampaing} />
         </SectionItem>
 
+        {/* subsection Tokens */}
         <SectionItem open={openTokens}>
           <GetTokens setOpen={setOpenTokens} />
         </SectionItem>
 
-        {/* submenu ClaimAll*/}
+        {/* subsection ClaimAll*/}
         <SectionItem open={openRewards}>
           <Rewards setOpen={setOpenRewards} />
         </SectionItem>
