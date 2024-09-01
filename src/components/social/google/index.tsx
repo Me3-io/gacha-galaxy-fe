2db2048eb7bf2b70076aa70324758fd83ea4bdf9
julietad <julieta.dangelo@7r1ck.com>
2024-09-01 14:40:37 -0300

    Style google

--- src/components/social/google/index.tsx
+++ src/components/social/google/index.tsx
@@ -1,42 +1,50 @@
 // src/GoogleLoginButton.tsx
 import React from 'react';
-import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { signInWithPopup, GoogleAuthProvider, getAuth } from 'firebase/auth';
 import { auth, provider } from 'config/firebaseConfig';
-import './styled.module.css';
+import styled from "./styled.module.scss";
 import { customAxiosLocalTest } from 'utils/customAxios';
+import icon from "assets/icons/google.png";
 
 const GoogleLoginButton: React.FC = () => {
 
   const fetchGoogle = async () => {
     try {
+      const auth = getAuth();
+      auth.currentUser?.getIdToken(/* forceRefresh */ true).then((idToken) => {
+        // Send idToken to your backend
+        console.log("Token:", idToken);
+        if (idToken) {
+          customAxiosLocalTest()
+            .post("/user/google", {
+              token: idToken
+            })
+            .then((response) => {
+              //setLoading(false);
+              alert(response);
+              if (response?.data?.status === "error") {
+  
+                //setShowAlertInfo({ open: true, text: response?.data?.message || "error" });
+              } else {
+                //setOpenSuccessModal(completeTelegram);
+              }
+            })
+            .catch((error) => {
+              //setLoading(false);
+              console.error(error);
+            });
+        }
+      }).catch((error) => {
+        console.error("Error retrieving ID token:", error);
+      })
       const result = await signInWithPopup(auth, provider);
-      const credential = GoogleAuthProvider.credentialFromResult(result); 
+      const credential = GoogleAuthProvider.credentialFromResult(result);
       const token = credential?.accessToken;
       const user = result.user;
 
       console.log("User Info:", user);
       console.log("Token:", token);
-
-      if (token) {
-        customAxiosLocalTest()
-        .post("/user/google", {
-          token: token
-        })
-        .then((response) => {
-          //setLoading(false);
-          alert(response);
-          if (response?.data?.status === "error") {
-            
-            //setShowAlertInfo({ open: true, text: response?.data?.message || "error" });
-          } else {
-            //setOpenSuccessModal(completeTelegram);
-          }
-        })
-        .catch((error) => {
-          //setLoading(false);
-          console.error(error);
-        });
-      }
+     
 
     } catch (error) {
       console.error("Login error:", error);
@@ -45,8 +53,8 @@ const GoogleLoginButton: React.FC = () => {
   };
 
   return (
-    <button className="google-login-button" onClick={fetchGoogle}>
-      <img src="google-icon.png" alt="Google Icon" className="google-icon" />
+    <button className={styled.googleLoginButton} onClick={fetchGoogle}>
+      <img src={icon} alt="Google" className={styled.googleIcon} />
       Login with Google
     </button>
   );
