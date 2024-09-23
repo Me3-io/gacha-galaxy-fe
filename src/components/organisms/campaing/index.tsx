5f6579ccd195e010ccaef9b70458e83d6b4bd94a
Luciano Raso <luciano.raso@7r1ck.com>
2024-09-23 11:44:49 -0300

    - campaing use thirdweb address

--- src/components/organisms/campaing/index.tsx
+++ src/components/organisms/campaing/index.tsx
@@ -9,8 +9,8 @@ import { MapContext } from "pages/home";
 
 import { useTranslation } from "react-i18next";
 import styled from "./styled.module.scss";
-import { useSelector } from "react-redux";
-import { getLeaderboard } from "reduxConfig/thunks/leaderboard";
+//import { useSelector } from "react-redux";
+//import { getLeaderboard } from "reduxConfig/thunks/leaderboard";
 
 const Campaing = () => {
   const { t } = useTranslation();
@@ -18,11 +18,11 @@ const Campaing = () => {
 
   const open = !!details?.claimrId;
 
-  const data = useSelector(getLeaderboard);
-  const address = data?.wallets.find((w: any) => w?.active)?.address || "";
+  //const data = useSelector(getLeaderboard);
+  //const address = data?.wallets.find((w: any) => w?.active)?.address || "";
 
   const [loading, setLoading] = useState(false);
-  const { signature, message } = JSON.parse(localStorage.getItem("session.account") || "{}");
+  const { address, signature, message } = JSON.parse(localStorage.getItem("session.account") || "{}");
 
   const onClose = (evt: any, reason: string) => {
     if (reason !== "backdropClick") {
