0aef406f718b5e8e95e5c6188262b408bab67967
matiascabeza <matiascabeza2012@gmail.com>
2024-10-23 16:49:56 -0300

    fix

--- src/pages/login/telegram/index.tsx
+++ src/pages/login/telegram/index.tsx
@@ -3,113 +3,109 @@ import { chain } from "config/thirdwebConfig";
 import { useConnect } from "thirdweb/react";
 import { Box, CircularProgress, Container } from "@mui/material";
 import { useNavigate, useParams } from "react-router-dom";
-import { inAppWallet } from "thirdweb/wallets";
 import Layout from "components/templates/layout";
 import styled from "../styled.module.scss";
-import { createThirdwebClient } from "thirdweb";
 import useAlert from "hooks/alertProvider/useAlert";
+import { client } from "config/thirdwebConfig";
+import { setSocial } from "reduxConfig/slices/social";
+import { useDispatch } from "react-redux";
+import { getProfiles, inAppWallet } from "thirdweb/wallets";
 
-
-const wallet = inAppWallet({
-  smartAccount: {
-    sponsorGas: true,
-    chain: chain,
-  },
-});
+const wallet = inAppWallet();
 
 const TelegramLogin = () => {
-  const { connect } = useConnect();
-  const { lang } = useParams();
-  const { signature, message } = useParams();
-  const { setAlert } = useAlert();
-  const errorMessage = "Error generating wallet";
-  const successMessage = " Generated wallet";
+    const { connect } = useConnect();
+    const { lang } = useParams();
+    const { signature, message } = useParams();
+    const { setAlert } = useAlert();
+    const errorMessage = "Error generating wallet";
+    const successMessage = " Generated wallet";
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-  const navigate = useNavigate();
+    const [error, setError] = useState(false);
+    const [isGenerateWallet, setIsGenerateWallet] = useState(false);
 
-  const [error, setError] = useState(false);
-  const [isGenerateWallet, setIsGenerateWallet] = useState(false);
+    useEffect(() => {
 
-  useEffect(() => {
-    if (!signature || !message) {
-      console.log("Missing signature or message");
-    } else {
-      const connectWallet = async () => {
-        try {
-          const walletOrFn = await connect(async () => {
-            try {
-              const client = createThirdwebClient({
-                clientId: "5c2008dc15fde34d454e47b09661e39d",
-              });
-              await wallet.connect({
-                client,
-                strategy: "auth_endpoint",
-                payload: JSON.stringify({
-                  signature: signature,
-                  message: message,
-                }),
-                encryptionKey: "00000000000",
-              });
-              console.log("Connection wallet:", wallet);
-              return wallet;
-            } catch (error) {
-              console.log("Connection error:", error);
-              setIsGenerateWallet(false);
-              return wallet;
-            }
-          });
-          if (walletOrFn) {
-            setIsGenerateWallet(true);
-            setError(false);
-            return true;
-          } else {
-            setIsGenerateWallet(false);
-            setError(true);
-            return false;
-          }
-        } catch (error) {
-          console.log("Connection error:", error);
-          setIsGenerateWallet(false);
-          setError(true);
-          return false;
+        if (!signature || !message) {
+            console.log('Missing signature or message');
+        } else {
+            const connectWallet = (async () => {
+                try {
+                    await wallet.connect({
+                        client,
+                        strategy: "auth_endpoint",
+                        payload: JSON.stringify({
+                            signature: signature,
+                            message: message,
+                        }),
+                        encryptionKey: '00000000000',
+                    });
+                    const walletOrFn = await connect(wallet)
+                    if (walletOrFn) {
+                        const account = walletOrFn?.getAccount();
+                        setIsGenerateWallet(true);
+                        setError(false);
+                        debugger
+                        const profiles = await getProfiles({ client });
+                        if (profiles) {
+                            const social = profiles[0]?.type || "";
+                            dispatch(setSocial(social) as any);
+                        } else {
+                            dispatch(setSocial("") as any);
+                        }
+                        return true;
+                    } else {
+                        setIsGenerateWallet(false);
+                        setError(true);
+                        return false;
+                    }
+
+
+                } catch (error) {
+                    console.log('Connection error:', error);
+                    setIsGenerateWallet(false);
+                    setError(true);
+                    return false;
+                }
+            })
+            connectWallet();
         }
-      };
-      connectWallet();
-    }
-  }, [signature, message]);
+    }, [signature]);
 
-  useEffect(() => {
-    if (error && !isGenerateWallet) {
-      navigate(`/${lang}/home`);
-      setAlert("Error generating wallet", "error");
-    }
-  }, [error, isGenerateWallet]);
+    useEffect(() => {
+        if (error && !isGenerateWallet) {
+           // navigate(`/${lang}/home`);
+            setAlert("Error generating wallet", "error");
+        }
+    }, [error, isGenerateWallet]);
 
-  useEffect(() => {
-    if (isGenerateWallet && !error) {
-      setAlert("Generated wallet", "success");
-    }
-  }, [isGenerateWallet, error]);
+    useEffect(() => {
+        if (isGenerateWallet && !error) {
+            setAlert("Generated wallet", "success");
+        }
+    }, [isGenerateWallet, error]);
 
-  return (
-    <Layout>
-      <Container maxWidth={false} disableGutters={true}>
-        <Box className={styled.main}>
-          <div
-            className="w-screen h-screen flex flex-col gap-2 items-center justify-center"
-            style={{ color: "aliceblue" }}
-          >
-            {!error && !isGenerateWallet && (
-              <Box className={styled.loading}>
-                <CircularProgress className={styled.spinner} size={36} />
-                Generating Wallet...{" "}
-              </Box>
-            )}
-          </div>
-        </Box>
-      </Container>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Container maxWidth={false} disableGutters={true}>
+                <Box className={styled.main}>
+                    <div
+                        className="w-screen h-screen flex flex-col gap-2 items-center justify-center"
+                        style={{ color: "aliceblue" }}
+                    >
+                        {!error && !isGenerateWallet && (
+                            <Box className={styled.loading}>
+                                <CircularProgress className={styled.spinner} size={36} />
+                                Generating Wallet...{" "}
+                            </Box>
+                        )}
+                    </div>
+                </Box>
+            </Container>
+        </Layout>
+    );
 };
 
 export default TelegramLogin;
